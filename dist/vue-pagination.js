0ae150ed9454da4b78e7b5dfae32e78d2b71b04a
luoye <842891024@qq.com>
2016-08-04 12:43:32 +0800

    sync only one config

--- dist/vue-pagination.js
+++ dist/vue-pagination.js
@@ -21,77 +21,81 @@
   	console.warn(e);
   };
 
+  var defaultOptions$1 = {
+  	firstStr: 'First',
+  	prevStr: 'Prev',
+  	nextStr: 'Next',
+  	lastStr: 'Last',
+  	first: true,
+  	prev: true,
+  	next: true,
+  	last: true,
+  	normal: true,
+  	currentPage: 1,
+  	itemsPerPage: 10,
+  	pagesLength: 5,
+  	totalItems: 0,
+  	onChange: function onChange() {}
+  };
+
   function vPagination (componentOptions) {
   	return {
   		name: 'vue-pagination',
-  		template: '<div class="pagination-con" v-show="resultOptions.totalItems !== 0">\n\t\t\t<ul>\n\t\t\t\t<li @click="changePage(1)" v-show="resultOptions.first && resultOptions.currentPage !== 1">{{resultOptions.firstStr}}</li>\n\t\t\t\t<li @click="prevPage()" v-show="resultOptions.prev" :class="{\'disabled\': resultOptions.currentPage === 1}">{{resultOptions.prevStr}}</li>\n\t\t\t\t<li @click="changePage(item)" v-show="resultOptions.normal" v-for="item in pageList" :class="{\'gap\': item === \'...\', \'active\': resultOptions.currentPage === item}" track-by="$index">{{item}}</li>\n\t\t\t\t<li @click="nextPage()" v-show="resultOptions.next" :class="{\'disabled\': resultOptions.currentPage === numberOfPages}">{{resultOptions.nextStr}}</li>\n\t\t\t\t<li @click="changePage(numberOfPages)" v-show="resultOptions.last && resultOptions.currentPage !== numberOfPages">{{resultOptions.lastStr}}</li>\n\t\t\t</ul>\n\t\t</div>',
+  		template: '<div class="pagination-con" v-show="paginationConfig.totalItems !== 0">\n\t\t\t<ul>\n\t\t\t\t<li @click="changePage(1)" v-show="paginationConfig.first && paginationConfig.currentPage !== 1">{{paginationConfig.firstStr}}</li>\n\t\t\t\t<li @click="prevPage()" v-show="paginationConfig.prev" :class="{\'disabled\': paginationConfig.currentPage === 1}">{{paginationConfig.prevStr}}</li>\n\t\t\t\t<li @click="changePage(item)" v-show="paginationConfig.normal" v-for="item in pageList" :class="{\'gap\': item === \'...\', \'active\': paginationConfig.currentPage === item}" track-by="$index">{{item}}</li>\n\t\t\t\t<li @click="nextPage()" v-show="paginationConfig.next" :class="{\'disabled\': paginationConfig.currentPage === numberOfPages}">{{paginationConfig.nextStr}}</li>\n\t\t\t\t<li @click="changePage(numberOfPages)" v-show="paginationConfig.last && paginationConfig.currentPage !== numberOfPages">{{paginationConfig.lastStr}}</li>\n\t\t\t</ul>\n\t\t</div>',
   		props: ['paginationConfig'],
   		data: function data() {
   			return {
   				componentOptions: componentOptions,
-  				defaultOptions: {
-  					firstStr: 'First',
-  					prevStr: 'Prev',
-  					nextStr: 'Next',
-  					lastStr: 'Last',
-  					first: true,
-  					prev: true,
-  					next: true,
-  					last: true,
-  					normal: true,
-  					currentPage: 1,
-  					itemsPerPage: 10,
-  					pagesLength: 5,
-  					totalItems: 0,
-  					onChange: function onChange() {}
-  				},
-  				resultOptions: {},
   				pageList: [],
   				numberOfPages: 0
   			};
   		},
+  		ready: function ready() {
+  			this.initConfig();
+  		},
+
   		methods: {
   			changePage: function changePage(page) {
   				if (page === '...') {
   					return;
   				}
-  				this.resultOptions.currentPage = page;
+  				this.paginationConfig.currentPage = page;
   			},
   			prevPage: function prevPage() {
-  				if (this.resultOptions.currentPage > 1) {
-  					this.resultOptions.currentPage -= 1;
+  				if (this.paginationConfig.currentPage > 1) {
+  					this.paginationConfig.currentPage -= 1;
   				}
   			},
   			nextPage: function nextPage() {
-  				if (this.resultOptions.currentPage < this.numberOfPages) {
-  					this.resultOptions.currentPage += 1;
+  				if (this.paginationConfig.currentPage < this.numberOfPages) {
+  					this.paginationConfig.currentPage += 1;
   				}
   			},
   			initConfig: function initConfig() {
-  				this.paginationConfig = this.paginationConfig === undefined ? {} : this.paginationConfig;
-  				this.resultOptions = mixin(this.defaultOptions, this.paginationConfig);
-  				this.numberOfPages = Math.ceil(this.resultOptions.totalItems / this.resultOptions.itemsPerPage);
-  				if (this.resultOptions.pagesLength % 2 === 0) {
-  					this.resultOptions.pagesLength = this.resultOptions.pagesLength - 1;
+  				if (this.paginationConfig === undefined) return;
+  				this.paginationConfig = mixin(defaultOptions$1, this.paginationConfig);
+  				this.numberOfPages = Math.ceil(this.paginationConfig.totalItems / this.paginationConfig.itemsPerPage);
+  				if (this.paginationConfig.pagesLength % 2 === 0) {
+  					this.paginationConfig.pagesLength = this.paginationConfig.pagesLength - 1;
   				}
   			},
   			generatePageList: function generatePageList() {
   				this.pageList = [];
   				var from = void 0,
   				    to = void 0;
-  				var offset = (this.resultOptions.pagesLength + 1) / 2;
-  				if (this.resultOptions.currentPage <= offset) {
+  				var offset = (this.paginationConfig.pagesLength + 1) / 2;
+  				if (this.paginationConfig.currentPage <= offset) {
   					from = 1;
-  					to = this.numberOfPages < this.resultOptions.pagesLength ? this.numberOfPages : this.resultOptions.pagesLength;
+  					to = this.numberOfPages < this.paginationConfig.pagesLength ? this.numberOfPages : this.paginationConfig.pagesLength;
   					while (from <= to) {
   						this.pageList.push(from);
   						from++;
   					}
-  					if (this.numberOfPages > this.resultOptions.pagesLength) {
+  					if (this.numberOfPages > this.paginationConfig.pagesLength) {
   						this.pageList.push('...');
   					}
-  				} else if (this.resultOptions.currentPage >= this.numberOfPages - offset) {
-  					from = this.numberOfPages - this.resultOptions.pagesLength;
+  				} else if (this.paginationConfig.currentPage >= this.numberOfPages - offset) {
+  					from = this.numberOfPages - this.paginationConfig.pagesLength;
   					to = this.numberOfPages;
   					while (from <= to) {
   						this.pageList.push(from);
@@ -99,8 +103,8 @@
   					}
   					this.pageList.unshift('...');
   				} else {
-  					from = this.resultOptions.currentPage - offset + 1;
-  					to = this.resultOptions.currentPage + offset - 1;
+  					from = this.paginationConfig.currentPage - offset + 1;
+  					to = this.paginationConfig.currentPage + offset - 1;
   					while (from <= to) {
   						this.pageList.push(from);
   						from++;
@@ -111,25 +115,22 @@
   			}
   		},
   		watch: {
-  			'paginationConfig.totalItems': {
+  			'paginationConfig': {
   				handler: function handler() {
   					this.initConfig();
   					this.generatePageList();
   				},
 
-  				immediate: true
+  				deep: true
   			},
-  			'resultOptions.currentPage': {
+  			'paginationConfig.currentPage': {
   				handler: function handler() {
-  					if (this.paginationConfig.currentPage !== this.resultOptions.currentPage) {
-  						this.generatePageList();
-  						this.paginationConfig.currentPage = this.resultOptions.currentPage;
-  						if (typeof this.paginationConfig.onChange !== 'function') {
-  							warn('Config of "onChange" must be a function!');
-  							return;
-  						}
-  						this.paginationConfig.onChange && this.paginationConfig.onChange();
+  					this.generatePageList();
+  					if (typeof this.paginationConfig.onChange !== 'function') {
+  						warn('Config of "onChange" must be a function!');
+  						return;
   					}
+  					this.paginationConfig.onChange && this.paginationConfig.onChange();
   				}
   			}
   		}
--- src/pagination.js
+++ src/pagination.js
@@ -1,84 +1,86 @@
 import { mixin, warn } from './util.js';
 
+let defaultOptions = {
+	firstStr: 'First',
+	prevStr: 'Prev',
+	nextStr: 'Next',
+	lastStr: 'Last',
+	first: true,
+	prev: true,
+	next: true,
+	last: true,
+	normal: true,
+	currentPage: 1,
+	itemsPerPage: 10,
+	pagesLength: 5,
+	totalItems: 0,
+	onChange: function() {}
+};
+
 export default function(componentOptions) {
 	return {
 		name: 'vue-pagination',
-		template:
-		`<div class="pagination-con" v-show="resultOptions.totalItems !== 0">
+		template: `<div class="pagination-con" v-show="paginationConfig.totalItems !== 0">
 			<ul>
-				<li @click="changePage(1)" v-show="resultOptions.first && resultOptions.currentPage !== 1">{{resultOptions.firstStr}}</li>
-				<li @click="prevPage()" v-show="resultOptions.prev" :class="{'disabled': resultOptions.currentPage === 1}">{{resultOptions.prevStr}}</li>
-				<li @click="changePage(item)" v-show="resultOptions.normal" v-for="item in pageList" :class="{'gap': item === '...', 'active': resultOptions.currentPage === item}" track-by="$index">{{item}}</li>
-				<li @click="nextPage()" v-show="resultOptions.next" :class="{'disabled': resultOptions.currentPage === numberOfPages}">{{resultOptions.nextStr}}</li>
-				<li @click="changePage(numberOfPages)" v-show="resultOptions.last && resultOptions.currentPage !== numberOfPages">{{resultOptions.lastStr}}</li>
+				<li @click="changePage(1)" v-show="paginationConfig.first && paginationConfig.currentPage !== 1">{{paginationConfig.firstStr}}</li>
+				<li @click="prevPage()" v-show="paginationConfig.prev" :class="{'disabled': paginationConfig.currentPage === 1}">{{paginationConfig.prevStr}}</li>
+				<li @click="changePage(item)" v-show="paginationConfig.normal" v-for="item in pageList" :class="{'gap': item === '...', 'active': paginationConfig.currentPage === item}" track-by="$index">{{item}}</li>
+				<li @click="nextPage()" v-show="paginationConfig.next" :class="{'disabled': paginationConfig.currentPage === numberOfPages}">{{paginationConfig.nextStr}}</li>
+				<li @click="changePage(numberOfPages)" v-show="paginationConfig.last && paginationConfig.currentPage !== numberOfPages">{{paginationConfig.lastStr}}</li>
 			</ul>
 		</div>`,
 		props: ['paginationConfig'],
 		data: function() {
 			return {
 				componentOptions: componentOptions,
-				defaultOptions: {
-					firstStr: 'First',
-					prevStr: 'Prev',
-					nextStr: 'Next',
-					lastStr: 'Last',
-					first: true,
-					prev: true,
-					next: true,
-					last: true,
-					normal: true,
-					currentPage: 1,
-					itemsPerPage: 10,
-					pagesLength: 5,
-					totalItems: 0,
-					onChange: function() {}
-				},
-				resultOptions: {},
 				pageList: [],
 				numberOfPages: 0
 			};
+		},
+		ready() {
+			this.initConfig();
 		},
 		methods: {
 			changePage(page) {
 				if (page === '...') {
 					return;
 				}
-				this.resultOptions.currentPage = page;
+				this.paginationConfig.currentPage = page;
 			},
 			prevPage() {
-				if (this.resultOptions.currentPage > 1) {
-					this.resultOptions.currentPage -= 1;
+				if (this.paginationConfig.currentPage > 1) {
+					this.paginationConfig.currentPage -= 1;
 				}
 			},
 			nextPage() {
-				if (this.resultOptions.currentPage < this.numberOfPages) {
-					this.resultOptions.currentPage += 1;
+				if (this.paginationConfig.currentPage < this.numberOfPages) {
+					this.paginationConfig.currentPage += 1;
 				}
 			},
 			initConfig() {
-				this.paginationConfig = this.paginationConfig === undefined ? {} : this.paginationConfig;
-				this.resultOptions = mixin(this.defaultOptions, this.paginationConfig);
-				this.numberOfPages = Math.ceil(this.resultOptions.totalItems / this.resultOptions.itemsPerPage);
-				if (this.resultOptions.pagesLength % 2 === 0) {
-					this.resultOptions.pagesLength = this.resultOptions.pagesLength - 1;
+				if (this.paginationConfig === undefined) return;
+				this.paginationConfig = mixin(defaultOptions, this.paginationConfig);
+				this.numberOfPages = Math.ceil(this.paginationConfig.totalItems / this.paginationConfig.itemsPerPage);
+				if (this.paginationConfig.pagesLength % 2 === 0) {
+					this.paginationConfig.pagesLength = this.paginationConfig.pagesLength - 1;
 				}
 			},
 			generatePageList() {
 				this.pageList = [];
 				let from, to;
-				let offset = (this.resultOptions.pagesLength + 1) / 2;
-				if (this.resultOptions.currentPage <= offset) {
+				let offset = (this.paginationConfig.pagesLength + 1) / 2;
+				if (this.paginationConfig.currentPage <= offset) {
 					from = 1;
-					to = this.numberOfPages < this.resultOptions.pagesLength ? this.numberOfPages : this.resultOptions.pagesLength;
+					to = this.numberOfPages < this.paginationConfig.pagesLength ? this.numberOfPages : this.paginationConfig.pagesLength;
 					while (from <= to) {
 						this.pageList.push(from);
 						from++;
 					}
-					if (this.numberOfPages > this.resultOptions.pagesLength) {
+					if (this.numberOfPages > this.paginationConfig.pagesLength) {
 						this.pageList.push('...');
 					}
-				} else if (this.resultOptions.currentPage >= this.numberOfPages - offset) {
-					from = this.numberOfPages - this.resultOptions.pagesLength;
+				} else if (this.paginationConfig.currentPage >= this.numberOfPages - offset) {
+					from = this.numberOfPages - this.paginationConfig.pagesLength;
 					to = this.numberOfPages;
 					while (from <= to) {
 						this.pageList.push(from);
@@ -86,8 +88,8 @@ export default function(componentOptions) {
 					}
 					this.pageList.unshift('...');
 				} else {
-					from = this.resultOptions.currentPage - offset + 1;
-					to = this.resultOptions.currentPage + offset - 1;
+					from = this.paginationConfig.currentPage - offset + 1;
+					to = this.paginationConfig.currentPage + offset - 1;
 					while (from <= to) {
 						this.pageList.push(from);
 						from++;
@@ -98,24 +100,21 @@ export default function(componentOptions) {
 			}
 		},
 		watch: {
-			'paginationConfig.totalItems': {
+			'paginationConfig': {
 				handler() {
 					this.initConfig();
 					this.generatePageList();
 				},
-				immediate: true
+				deep: true
 			},
-			'resultOptions.currentPage': {
+			'paginationConfig.currentPage': {
 				handler() {
-					if (this.paginationConfig.currentPage !== this.resultOptions.currentPage) {
-						this.generatePageList();
-						this.paginationConfig.currentPage = this.resultOptions.currentPage;
-						if (typeof this.paginationConfig.onChange !== 'function') {
-							warn('Config of "onChange" must be a function!');
-							return;
-						}
-						this.paginationConfig.onChange && this.paginationConfig.onChange();
+					this.generatePageList();
+					if (typeof this.paginationConfig.onChange !== 'function') {
+						warn('Config of "onChange" must be a function!');
+						return;
 					}
+					this.paginationConfig.onChange && this.paginationConfig.onChange();
 				}
 			}
 		}
